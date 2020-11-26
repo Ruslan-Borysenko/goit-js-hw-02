bfafae7eff8dcedfe020977b146c3f864c0cfd36
Ruslan-Borysenko <ruslanborisenkona@gmail.com>
2020-11-26 18:43:11 +0200

    add 1 task

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const logItem = function (array) {
+    for (let i = 0; i < array.length; i += 1) {
+        return console.log(`${i + 1} - ${array[i]}`);
+    }
+}
+
+logItem(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+logItem([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
