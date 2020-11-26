b6e893f5962cacadcbef8f4350d52f3052c63604
yegorgangalo <71293418+yegorgangalo@users.noreply.github.com>
2020-11-26 21:46:28 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -6,6 +6,12 @@ const calculateEngravingPrice = function (message, pricePerWord) {
     message = (message.split(' ').length);
     let result;
     console.log(result = pricePerWord * message);
+    //----я б так написав. Так більш читабельно
+    //const wordArray = message.split(' '); //так в самій назві зрозуміло, що створено масив зі слів. Перевизначати змінні можна і Репета так часом робитиме, але потім він скаже, що так робити не варто, бо через це часом при винекненні помилки в коді важко зрозуміти яка змінна дає збій
+    //const wordArrayLength = wordArray.length;
+    //const result = pricePerWord * wordArrayLength;
+    //console.log(result);
+    //так легше читається код. Іншій людині легше розібратись, що ти написав)
 }
 
 calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10);
