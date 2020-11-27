bf9cf2828505e5727ecb8edd5f15385bf9fb339c
Ruslan-Borysenko <ruslanborisenkona@gmail.com>
2020-11-27 15:49:26 +0200

    changed method of solving 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -2,20 +2,14 @@
 // 2. Перевірити рядок на слова: spam, sale.
 // 3. Якщо знайшли слово - вивожу true в ін. випадку - false
 
-
 const checkForSpam = function (message) {
-    const word1 = message.toLowerCase().indexOf('spam');
-    const word2 = message.toLowerCase().indexOf('sale');
-    if (word1 === -1 && word2 === -1) {
-        console.log('false');
-    } else {
-        console.log('true');
-    }
+  message = message.toLowerCase();
+  const result =
+    message.includes('spam') || message.includes('sale') ? 'true' : 'false';
+  console.log(result);
 };
 
 checkForSpam('Latest technology news'); // false
 checkForSpam('JavaScript weekly newsletter'); // false
 checkForSpam('Get best sale offers now!'); // true
 checkForSpam('[SPAM] How to earn fast money?'); // true
-
-
--- js/task-6.js
+++ js/task-6.js
@@ -12,22 +12,22 @@ let input;
 const numbers = [];
 let total = 0;
 
-for (; ;) {
-    input = prompt('Введите число:');
-    let number = Number(input);
+for (;;) {
+  input = prompt('Введите число:');
+  let number = Number(input);
 
-    if (input === null) {
-        for (let i = 0; i < numbers.length; i += 1) {
-            total += numbers[i];
-        }
-        console.log(`Общая сумма чисел равна ${total}`);
-        console.log(numbers);
-        break;
+  if (input === null) {
+    for (let i = 0; i < numbers.length; i += 1) {
+      total += numbers[i];
     }
+    console.log(`Общая сумма чисел равна ${total}`);
+    console.log(numbers);
+    break;
+  }
 
-    if (Number.isNaN(number) === false) {
-        numbers.push(number);
-    } else {
-        alert("Было введено не число, попробуйте еще раз");
-    }
+  if (Number.isNaN(number) === false) {
+    numbers.push(number);
+  } else {
+    alert('Было введено не число, попробуйте еще раз');
+  }
 }
